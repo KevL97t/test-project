942991ecdb91b2d415b2e320c78c43da24b1467f
kevl97t <kalarboleda97@mail.com>
2021-09-02 19:19:50 -0500

    Terminación funcional del primer taller

--- figuras.js
+++ figuras.js
@@ -73,3 +73,111 @@ function areaCirculo(radio)
 // console.log("El área del circulo es: " + areaCirculo + "cm");
 
 console.groupEnd();
+
+
+// AQUI INTERACTUAMOS CON EL HTML
+
+function calcularPerimetroCuadrado()
+{
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const perimetro = perimetroCuadrado(value);
+  alert(perimetro);
+
+}
+function calcularAreaCuadrado()
+{
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
+
+  const area = areaCuadrado(value);
+  alert(area);
+
+}
+
+function calcularPerimetroTriangulo()
+{
+    const lado1 = document.getElementById("lado1");
+    const valorlado1 = lado1.value;
+    const valorlado1entero = parseInt(valorlado1);
+
+    const lado2 = document.getElementById("lado2");
+    const valorlado2 = lado2.value;
+    const valorlado2entero = parseInt(valorlado2);
+
+    const base = document.getElementById("base");
+    const valorbase = base.value;
+    const valorbaseentero = parseInt(valorbase);
+    
+
+    const perimetro = perimetroTriangulo(valorlado1entero, valorlado2entero, valorbaseentero);
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo()
+{
+    const base = document.getElementById("base");
+    const valorbase = base.value;
+    const valorbaseentero = parseInt(valorbase);
+
+    const altura = document.getElementById("altura");
+    const valoraltura = altura.value;
+    const valoralturaentero = parseInt(valoraltura);
+
+    const area = areaTriangulo(valorbaseentero, valoralturaentero);
+    alert(area);
+}
+
+function calcularDiametro()
+{
+    const radio = document.getElementById("radio");
+    const valorradio = radio.value;
+
+    const diametro = diametroCirculo(valorradio);
+    alert(diametro);
+}
+
+function calcularPerimetroCirculo()
+{
+    const radio = document.getElementById("radio");
+    const valorradio = radio.value;
+
+    const perimetro = perimetroCirculo(valorradio);
+    alert(perimetro);
+}
+
+function calcularAreaCirculo()
+{
+    const radio = document.getElementById("radio");
+    const valorradio = radio.value;
+
+    const area = areaCirculo(valorradio);
+    alert(area);
+}
+
+function calcularAlturaTrianguloIsoceles()
+{
+    const lado1 = document.getElementById("lado1iso");
+    const valorlado1 = lado1.value;
+    const valorlado1entero = parseInt(valorlado1);
+
+    const lado2 = document.getElementById("lado2iso");
+    const valorlado2 = lado2.value;
+    const valorlado2entero = parseInt(valorlado2);
+
+    const base = document.getElementById("baseiso");
+    const valorbase = base.value;
+    const valorbaseentero = parseInt(valorbase);
+
+    if(valorlado1entero == valorlado2entero && valorlado1entero != valorbaseentero || valorlado1entero == valorbaseentero && valorlado1entero != valorlado2entero || valorlado2entero == valorbaseentero && valorlado2entero != valorlado1entero)
+    {
+
+        const resultado = Math.sqrt((valorlado1entero**2) - ((valorbaseentero**2)/4) );
+        alert("Efectivamente, es un triángulo isóceles y la altura es de: " + resultado);
+    }
+    else
+    {
+        alert("Los valores indicados no corresponden a un triángulo isóceles");
+    }
+}
